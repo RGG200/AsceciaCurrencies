bb7d35230ac3e769c4e557896b4003c005f91b3b
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-01-24 19:00:30 +0100

    V1.0S

---
+++ AsceciaCurrencies/src/main/java/me/rgn/asceciacurrencies/Currency.java
@@ -0,0 +1,11 @@
+package me.rgn.asceciacurrencies;
+
+public class Currency
+{
+    public static boolean isCurrencyCreated;
+    public static String CurrencyName;
+    public static int CurrencyTotalValue;
+    public static int CurrencyAmount;
+    public static double CurrencyPower;
+
+}
